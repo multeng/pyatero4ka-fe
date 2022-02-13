9dc0fcbf114c9b4a618d2331af92fc9063481577
Vladimir Kormakov <Vladimir_Kormakov@epam.com>
2022-02-13 23:16:41 +0300

    feat: task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://6mmm2p4cgj.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://hlbxxdwx0l.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://62aic0i8wb.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://6mmm2p4cgj.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
